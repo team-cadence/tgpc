77e0ee36736b1099641cba716f03af3202f2940e
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 19:31:29 +0200

    Closer

--- areal.c
+++ areal.c
@@ -1,1 +1,1 @@
-f=1,i,w,h;p(n){for(i=2;i<n;)if(n%++i==0&&i!=n)f=0;return f;}main(){scanf("%d %d",&w,&h);printf("%s\n",p(w)&p(h)?"Nei":"Ja");exit(0);}
+i,w,h;p(n){for(i=2;i<n;)if(n%++i==0&&i!=n)w=0;return w;}main(){scanf("%d %d",&w,&h);printf("%s\n",p(w)&p(h)?"Nei":"Ja");exit(0);}
