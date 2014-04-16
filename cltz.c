de6bf80fc87ab826a2a27f36fa4c53f2d048ba15
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:02:43 +0200

    stuff works

--- cltz.c
+++ cltz.c
@@ -11,12 +11,7 @@ int main(int argc, char** args) {
 
 	while (n != b) {
 		v++;
-
-		if (n % 2 == 0)
-			n /= 2;
-		else 
-			n = 3 * n + 1;
-
+		n=(n%2)?3*n+1:n/2;	
 		if (n == b) {
 			printf("%d\n", v);
 			return 0;
