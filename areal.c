53713ec1ef6166420886d084855638072743863a
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 18:17:14 +0200

    Moved

--- areal.c
+++ areal.c
@@ -1,14 +1,13 @@
-int w,h,i;
-int isPrime(int number) {
-	if (number < 2) return 0;
-	for (i = 2; i < number; i++) {
-        if (number % i == 0 && i != number) return 0;
-    }
+#include <stdio.h>
 
-    return 1;
-}
+int i,w,h;
 
-main() {
-	scanf("%d %d",&w,&h);
-	printf("%s\n", (isPrime(w)&isPrime(h))?"Nei":"Ja");
+int p(int number) {for (i = 2; i < number; i++)if (number % i == 0 && i != number) return 0;return 1;}
+
+int main() {
+	scanf("%d %d", &w, &h);
+
+	printf("%s\n", (p(w) && p(h)) ? "Nei" : "Ja");
+
+	return 0;
 }
