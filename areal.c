772f9620d19e317fb898aeb6afa7b47c7e2f701f
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 18:24:16 +0200

    Shorter is always better

--- areal.c
+++ areal.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-
 int i,w,h;
-
-int p(int number) {for (i = 2; i < number; i++)if (number % i == 0 && i != number) return 0;return 1;}
-
-int main() {
+int p(int n) {for(i=2;i<n;i++)if(n%i==0&&i!=n)return 0;return 1;}
+main() {
 	scanf("%d %d", &w, &h);
 
 	printf("%s\n", (p(w) && p(h)) ? "Nei" : "Ja");
