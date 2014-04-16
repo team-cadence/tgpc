5af71528b086f829be355723910028254818e18a
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 19:05:53 +0200

    This is fucking madness

--- areal.c
+++ areal.c
@@ -1,1 +1,1 @@
-int i,w,h;int p(int n){for(i=2;i<n;i++)if(n%i==0&&i!=n)return 0;return 1;}main(){scanf("%d %d",&w,&h);printf("%s\n",p(w)&p(h)?"Nei":"Ja");exit(0);}
+i,w,h;p(n){for(i=2;i<n;)if(n%++i==0&&i!=n)return 0;return 1;}main(){scanf("%d %d",&w,&h);printf("%s\n",p(w)&p(h)?"Nei":"Ja");exit(0);}
--- cltz.c
+++ cltz.c
@@ -1,2 +1,2 @@
 #define _(x) {printf("%d\n",x);exit(0);}
-int m,n,b,v;main(){scanf("%d %d %d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)_(-1)_(v)}
+m,n,b,v;main(){scanf("%d %d %d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)_(-1)_(v)}
