c7e6e3b80251affb08047e392fdaf53c2f55d239
Theneva <splugs@gmail.com>
2014-04-17 00:08:36 +0200

    126 cltz

--- cltz.c
+++ cltz.c
@@ -1,2 +1,1 @@
-#define _(x) {printf("%d\n",x);exit(0);}
-m,n,b,v;main(){scanf("%d %d %d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)_(-1)_(v)}
+p(k){printf("%d\n",k);exit(0);}m,n,b,v;main(){scanf("%d%d%d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)p(-1);p(v);}
