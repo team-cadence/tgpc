cd5a51d65271f16ac5ac3a6a949c37dabdb6ab96
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:04:14 +0200

    Madness is bestowed upon us

--- cltz.c
+++ cltz.c
@@ -2,21 +2,9 @@
 #include <stdlib.h>
 
 int main(int argc, char** args) {
-	int m, n, b;
-
+	int m,n,b,v=0;
 	scanf("%d %d %d", &m, &n, &b);
-
-	int v = 0;
-
-
-	while (n != b) {
-		v++;
-		n=(n%2)?3*n+1:n/2;	
-		if (n == b) {
-			printf("%d\n", v);
-			return 0;
-		}
-
+	for(;n!=b;v++,n=(n%2)?3*n+1:n/2){
 		if (n <= 1 || n > m) {
 			printf("%d\n", -1);
 			return 0;
