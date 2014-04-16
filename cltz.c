736aecfeed74b93768786c0f2fc63bc267636860
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:10:49 +0200

    Fun

--- cltz.c
+++ cltz.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define _(x) printf("%d\n",x);return 0;
 main() {
 	int m,n,b,v=0; scanf("%d %d %d", &m, &n, &b);
 	for(;n!=b;v++,n=(n%2)?3*n+1:n/2){
 		if (n <= 1 || n > m) {
-			printf("%d\n", -1);
-			return 0;
+			_(-1)
 		}
 	}
-	printf("%d\n", v);
-	return 0;
+	_(v)
 }
