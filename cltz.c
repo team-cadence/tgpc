e02089c8a37d67d435055b64a3eedddb855e191a
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:17:16 +0200

    Nightmares will follow this day

--- cltz.c
+++ cltz.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define _(x) printf("%d\n",x);return 0;
-main() {
-	int m,n,b,v=0; scanf("%d %d %d", &m, &n, &b);
-	for(;n!=b;v++,n=(n%2)?3*n+1:n/2){
-		if (n <= 1 || n > m) {
-			_(-1)
-		}
-	}
+#define _(x) {printf("%d\n",x);return 0;}
+main(){
+	int m,n,b,v=0;scanf("%d %d %d",&m,&n,&b);
+	for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<=1||n>m)_(-1)
 	_(v)
 }
