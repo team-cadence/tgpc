be485c11c15e938ca2539fb24fa1a9bbcd7789ab
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:53:06 +0200

    Even more madness

--- cltz.c
+++ cltz.c
@@ -1,2 +1,2 @@
 #define _(x) {printf("%d\n",x);exit(0);}
-main(){int m,n,b,v=0;scanf("%d %d %d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)_(-1)_(v)}
+int m,n,b,v;main(){scanf("%d %d %d",&m,&n,&b);for(;n!=b;v++,n=(n%2)?3*n+1:n/2)if(n<2|n>m)_(-1)_(v)}
