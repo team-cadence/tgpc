f4ccd2a39589c9f85e08795f7bd8ac9e72003a29
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 18:29:14 +0200

    This is sick

--- areal.c
+++ areal.c
@@ -1,1 +1,1 @@
-int i,w,h;int p(int n){for(i=2;i<n;i++)if(n%i==0&&i!=n)return 0;return 1;}main(){scanf("%d %d",&w,&h);printf("%s\n",(p(w)&p(h))?"Nei":"Ja");return 0;}
+int i,w,h;int p(int n){for(i=2;i<n;i++)if(n%i==0&&i!=n)return 0;return 1;}main(){scanf("%d %d",&w,&h);printf("%s\n",p(w)&p(h)?"Nei":"Ja");exit(0);}
