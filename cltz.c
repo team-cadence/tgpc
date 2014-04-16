9361d9d2895ec113e97dbb923968ac44b6e49858
Arne-Christian Blystad <arnechristian@pixavi.com>
2014-04-16 17:05:26 +0200

    Even shorter

--- cltz.c
+++ cltz.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** args) {
-	int m,n,b,v=0;
-	scanf("%d %d %d", &m, &n, &b);
+main() {
+	int m,n,b,v=0; scanf("%d %d %d", &m, &n, &b);
 	for(;n!=b;v++,n=(n%2)?3*n+1:n/2){
 		if (n <= 1 || n > m) {
 			printf("%d\n", -1);
 			return 0;
 		}
 	}
-
 	printf("%d\n", v);
-
 	return 0;
 }
